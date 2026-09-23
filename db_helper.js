c2a5dbac02df49328aecca0cdbbe07b75a5c47c8
Yan Bunko <yanbunko@gmail.com>
2026-09-23 16:47:02 +0300

    Auto-update: 23.09.2026 16:47:02,62

--- db_helper.js
+++ db_helper.js
@@ -208,7 +208,7 @@ module.exports = {
           const freePoints = safeReadField(cloudPlayer, pointsKey, 0);
 
           const totalFighterPoints = str + agi + end + lck + freePoints;
-          const maxLegalPoints = 5 + 8 + ((correctLevel - 1) * 8); 
+          const maxLegalPoints = 8 + 8 + ((correctLevel - 1) * 8); 
 
           let needsDbSync = false;
           let updatePayload = {};
@@ -222,7 +222,7 @@ module.exports = {
             });
 
             updatePayload[cloudPlayer.level !== undefined ? 'level' : 'level'] = correctLevel;
-            updatePayload[pointsKey] = maxLegalPoints - 5; 
+            updatePayload[pointsKey] = maxLegalPoints - 8; 
             updatePayload['hp'] = getServerMaxHp({ endurance: 1, equipped: cloudPlayer.equipped || {} });
 
             needsDbSync = true;
